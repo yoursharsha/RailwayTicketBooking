9b219fae5b2753da1944e6b5a5163ec3419f74a3
Sriharsha <yoursharsha@gmail.com>
2020-04-23 21:01:54 +0530

    added @repository

--- Data/src/main/java/org/railway/ticketbooking/DAO/CustomerDAO.java
+++ Data/src/main/java/org/railway/ticketbooking/DAO/CustomerDAO.java
@@ -2,10 +2,13 @@ package org.railway.ticketbooking.DAO;
 
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
+
+import java.math.BigInteger;
+
 import org.railway.ticketbooking.Data.CustomerTable;
 import org.springframework.data.repository.CrudRepository;
 
 @Repository
-public interface CustomerDAO extends CrudRepository<CustomerTable, String> {
+public interface CustomerDAO extends CrudRepository<CustomerTable, BigInteger> {
 	
 }
